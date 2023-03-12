7ff8785da0fbd15313ae93ea9863927c7c526ece
Darcy Luís Neves Brás da Silva <stardevelin@gmail.com>
2023-03-12 01:09:16 +0000

    forgot to use the flag -w and used > instead, resulting into incorrect port, just re rant with -w flag

--- SiteCheck.py
+++ SiteCheck.py
@@ -1,30 +1,65 @@
---- SiteCheck.py	(original)
-+++ SiteCheck.py	(refactored)
-@@ -25,7 +25,7 @@
-             if len(diff.strip()) > 0:
-                 sitesWithDiff[site] = diff.strip()
- 
--    print "Found",len(sitesWithDiff), "Sites with diffs"
-+    print("Found",len(sitesWithDiff), "Sites with diffs")
- 
-     # construct the email notice
-     if len(sitesWithDiff) > 0:
-@@ -43,8 +43,8 @@
- 	subject += " " + strftime("%d.%m.%Y")
-         text += "SiteCheck.py - " + VERSION
- 
--        print subject
--        print text
-+        print(subject)
-+        print(text)
- 
- 	# Send the mail to every address in mails.txt
-         mails = open("mails.txt", "r").readlines()
-@@ -60,6 +60,6 @@
-     try:
-         sys.exit(checkSites())
-     except Exception as err:
--	print err # Stupid but gets the job done...
-+	print(err) # Stupid but gets the job done...
- 	sys.exit(1)
- 
+'''
+Checks a list of urls for changes.
+
+See README for more information.
+'''
+import sys
+from time import strftime
+
+import sendmail
+import sites
+
+VERSION = "1.3"
+
+def checkSites():
+    """ Start the site checking business. """
+
+    siteFile = open("sites.txt", "r")
+    sitesLines = siteFile.readlines()
+    siteDict = sites.processSitesInFile(sitesLines)
+
+    sitesWithDiff = {}
+    for site in siteDict:
+        diff = sites.checkSite(siteDict, site)
+        if diff is not None:
+            if len(diff.strip()) > 0:
+                sitesWithDiff[site] = diff.strip()
+
+    print("Found",len(sitesWithDiff), "Sites with diffs")
+
+    # construct the email notice
+    if len(sitesWithDiff) > 0:
+        subject = "Observer Report - "
+        text = "Observed Changes:\n"
+        for site in sitesWithDiff:
+            subject += site + ", "
+
+            text += "~"*10 + " " + site + " - " + siteDict[site]
+            text += " " + "~"*10 + "\n"
+            text += sitesWithDiff[site]
+            text += "\n\n"
+
+        subject = subject[:-2]
+	subject += " " + strftime("%d.%m.%Y")
+        text += "SiteCheck.py - " + VERSION
+
+        print(subject)
+        print(text)
+
+	# Send the mail to every address in mails.txt
+        mails = open("mails.txt", "r").readlines()
+        sendmail.connectToServer()
+
+        for mail in mails:
+	    mail = mail.strip()
+            if len(mail) > 0 and not mail.startswith("#"):
+                sendmail.sendmail(mail, subject, text)
+        sendmail.closeConnection()
+
+if __name__ == "__main__":
+    try:
+        sys.exit(checkSites())
+    except Exception as err:
+	print(err) # Stupid but gets the job done...
+	sys.exit(1)
+
